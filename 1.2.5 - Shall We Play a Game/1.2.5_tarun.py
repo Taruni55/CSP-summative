1dafdc45aa6586b182c3aa904eb21cac462bf485
Taruni55 <235176@shakopeeschools.org>
2025-10-27 11:16:23 -0500

    Update 1.2.5_tarun.py
    added modules and got started on the number choosing process for the game

--- 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
@@ -1,9 +1,21 @@
 #import modules
+import turtle as trtl
+import random as rand
 
 #draw screen template
+draw = trtl.Turtle()
 
 #game functions
+number_values = [0,1,2,3,4,5,6,7,8,9]
+chips = 5000
+wins = 0
+losses = 0 
 
+def draw_numbers():
+    card_1 = rand.randint(number_values)
+    print(card_1)
+    
+draw_numbers()
 #loop for main game
 
 #bet size
