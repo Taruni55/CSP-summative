6f82515b39b6da2fbcb2ceccf34161025a7ce351
Taruni55 <235176@shakopeeschools.org>
2025-10-27 10:33:37 -0500

    Create 1.2.5_tarun.py
    Added notes on each section of code

---
+++ 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
@@ -0,0 +1,21 @@
+#import modules
+
+#draw screen template
+
+#game functions
+
+#loop for main game
+
+#bet size
+
+#choose player or banker
+
+
+#deal cards
+
+#check if eligible to draw 3rd card
+
+#check winner
+
+
+#end game when out of chips
