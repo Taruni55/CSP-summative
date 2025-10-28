17f958442d98e652b2660c79dcf7c5c91857ec24
Taruni55 <235176@shakopeeschools.org>
2025-10-28 12:43:45 -0500

    baccarat
    added pics and set variables

--- 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
@@ -4,7 +4,8 @@ import random as rand
 
 #draw screen template
 draw = trtl.Turtle()
-
+screen = trtl.Screen()
+screen.bgcolor("green")
 #game functions
 number_values = [0,1,2,3,4,5,6,7,8,9]
 chips = 5000
@@ -12,8 +13,15 @@ wins = 0
 losses = 0 
 
 def draw_numbers():
-    card_1 = rand.randint(number_values)
-    print(card_1)
+    card_1 = rand.choice(number_values)
+    card_2 = rand.choice(number_values)
+    card_3 = rand.choice(number_values)
+    card_4 = rand.choice(number_values)
+    card_5 = rand.choice(number_values)
+    card_6 = rand.choice(number_values)
+    
+    
+    
     
 draw_numbers()
 #loop for main game
