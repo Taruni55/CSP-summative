493399af50be945920b275a7ec910b60533e577a
Taruni55 <235176@shakopeeschools.org>
2025-11-02 18:15:27 -0600

    new definitions
    added defintions on determineing the value of the cards, and also drawing out the cards because I couldn't get image resizing to work

--- 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
+++ 1.2.5 - Shall We Play a Game/1.2.5_tarun.py
@@ -1,17 +1,47 @@
 #import modules
 import turtle as trtl
 import random as rand
+import time
 
 #draw screen template
 draw = trtl.Turtle()
 screen = trtl.Screen()
 screen.bgcolor("green")
+
 #game functions
+draw.hideturtle()
+draw.speed(0)
+draw.pensize(3) 
+
 number_values = [0,1,2,3,4,5,6,7,8,9]
 chips = 5000
 wins = 0
 losses = 0 
 
+def baccarat_value(cards):
+    return sum(cards) % 10 
+
+def draw_card(x, y, value, label):
+    draw.penup()
+    draw.goto(x, y)
+    draw.pendown()
+    draw.color("white")
+    draw.begin_fill()
+    for _ in range(2):
+        draw.forward(60)
+        draw.right(90)
+        draw.forward(90)
+        draw.right(90)
+    draw.end_fill()
+    draw.penup()
+    draw.goto(x + 25, y - 50)
+    draw.color("black")
+    draw.write(value, align="center", font=("Arial", 24, "bold"))
+    draw.goto(x + 30, y - 100)
+    draw.write(label, align="center", font=("Arial", 10, "normal"))
+    screen.update()
+    time.sleep(1) 
+    
 def draw_numbers():
     card_1 = rand.choice(number_values)
     card_2 = rand.choice(number_values)
@@ -19,9 +49,7 @@ def draw_numbers():
     card_4 = rand.choice(number_values)
     card_5 = rand.choice(number_values)
     card_6 = rand.choice(number_values)
-    
-    
-    
+
     
 draw_numbers()
 #loop for main game
@@ -39,3 +67,6 @@ draw_numbers()
 
 
 #end game when out of chips
+
+wn=trtl.Screen()
+wn.mainloop()
