eff4a5e29e23e04c31acd143fe62a5315398cf41
Taruni55 <235176@shakopeeschools.org>
2025-11-11 10:16:45 -0600

    Update 1.3.1.tarun_and_anna.py
    decomposed

--- 1.3.1 - Artistic Expression/1.3.1.tarun_and_anna.py
+++ 1.3.1 - Artistic Expression/1.3.1.tarun_and_anna.py
@@ -3,3 +3,20 @@ import math
 import time
 import random as rand
 
+#game functions
+
+#click envelope to open letter
+
+#get users name
+
+#generate random message
+
+#draw out shape for coloring sheet
+
+#generate math questions when clicked
+
+#choose fill color when correct
+
+#when done, just display image at end
+
+#maybe add option to play again
